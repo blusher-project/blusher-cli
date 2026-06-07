8cdbd9579d470ea83dc3c9af1a0e944b25ae7561
Aspen Schneider <rendezvous71@outlook.com>
2026-06-07 18:20:14 +0900

    Add an install script.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blusher CLI
+
+## Installation
+
+```sh
+$ curl sh -c "$(curl -fsSL https://raw.githubusercontent.com/blusher-project/blusher-cli/main/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# https://raw.githubusercontent.com/blusher-project/blusher-cli/main/install.sh
+
+git clone https://github.com/blusher-project/blusher-cli.git $HOME/.blusher
+
+echo '==============================================================='
+echo 'Copy and paste the code below into your .bashrc or .zshrc file.'
+echo ''
+echo 'export BLUSHER_ROOT="$HOME/.blusher"'
+echo '[[ -d $BLUSHER_ROOT/bin ]] && export PATH="$BLUSHER_ROOT/bin:$PATH"'
 
